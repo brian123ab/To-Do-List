1f31fbf780d914c4b4a837681a96e6a44f74d00f
brian123ab <idki74386@gmail.com>
2025-05-10 13:29:48 +0000

    Added a register function

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -27,3 +27,42 @@ def login():
             "email": user.email
         }
     })
+
+
+@auth.route["/register", methods=["POST"]]
+def register():
+    data = requests.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    if not all([username, email, password]):
+        return jsonify ({"error": "Missing fields: username, password, and/or email"}), 400
+    
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "Username taken"}), 400
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "Email taken"}), 400
+    
+    new_user = User(
+        username=username,
+        email=eamil,
+        first_name=first_name,
+        last_name=last_name
+    )
+    new_user.set_password(password)
+    db.session.add(new_user)
+
+    db.session.commit()
+    return jsonify({
+        "message": "Registration succesful",
+        "user": {
+            "id": new_user.user_id,
+            "username": new_user.username,
+            "email": new_user.email.,
+            "first_name": new_user.first_name,
+            "last_name": new_user.last_name
+
+        }
+    })
